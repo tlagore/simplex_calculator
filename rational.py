8c9b02413669de77ea321c5cb47da4474fc02509
Tyrone Lagore <tyronelagore@gmail.com>
2022-06-09 10:13:50 -0700

    create Rational class that represents a Rational number and supports all operations

---
+++ rational.py
@@ -0,0 +1,295 @@
+from decimal import DivisionByZero
+import math
+
+from typing import Type
+
+class Rational():
+    """
+    Describes a rational number of the form p/q where both p and q are whole integers.
+
+    Supports all operations, addition, subtraction, division, multiplication, and equality.
+
+    Every pythonic operator is overridden for the above operations, 
+    i.e: + += - -= / /= * *= and ==
+
+    Note that == returns true if the 2 rational numbers *represent the same rational number*, 
+    which is true for fractions like (2/4 == 1/2). 
+    """
+    def __init__(self, numerator, denominator=1):
+        if denominator <= 0 or denominator is None or not isinstance(denominator, int):
+            raise Exception("Denominator must be an integer, " +
+                "cannot be less than or equal to 0, and cannot be None. " +
+                "Use '1' for whole numbers. Make the numerator negative for negative rationals.")
+
+        if numerator is None or not isinstance(numerator, int):
+            raise Exception("Numerator must be an integer")
+
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __subtract(self, other: Type['Rational'], in_place) -> 'Rational':
+        print(f"SUBTRACTING: {self} - {other}. In place: {in_place}")
+        subtractor = Rational(-other.numerator, other.denominator)
+        print(f"SUBTRACTED: {self}")
+
+        return self.__add(subtractor, in_place)
+
+    def __add(self, other: Type['Rational'], in_place):
+        print(f"ADDING: {self} + {other}, In place: {in_place}")
+
+        if self.denominator == other.denominator:
+            # covers whole numbers (both None) and shared denominators
+            if in_place:
+                self.numerator += other.numerator
+            else:
+                return Rational(self.numerator + other.numerator, self.denominator)
+        else:
+            new_denominator = self.__lcm(self.denominator, other.denominator)
+
+            # we don't mutate here
+            adder = self.__change_denominator(new_denominator)
+            addend = other.__change_denominator(new_denominator)
+
+            summand = Rational(adder.numerator + addend.numerator, new_denominator)
+            summand.simplify()
+
+            print(f"ADDED: {summand}")
+            
+            if in_place:
+                self.numerator = summand.numerator
+                self.denominator = summand.denominator
+                return self
+            else:
+                return summand
+
+    def __divide(self, divisor: Type['Rational'], in_place) -> 'Rational':
+        """
+        Divide two rationals by flipping the numerator and denominator and multiplying
+        """
+        if divisor.numerator == 0:
+            raise DivisionByZero(f"Cannot perform division as divisor has numerator of {divisor.numerator}")
+
+        print(f"DIVIDING: {self} / {divisor}, In place: {in_place}")
+
+        # flip the divisor, maintain the sign of the numerator
+        new = Rational(int(math.copysign(divisor.denominator, divisor.numerator)), abs(divisor.numerator))
+        
+        print(f"DIVIDED: {new}")
+        return self.__multiply(new, in_place)
+    
+    def __multiply(self, other: Type['Rational'], in_place) -> 'Rational':
+        """
+        Multiply two rationals
+        """
+        print(f"MULTIPLYING: {self} * {other}, In place: {in_place}")
+        if in_place:
+            new = self
+        else:
+            new = Rational(self.numerator, self.denominator)
+
+        new.numerator = new.numerator * other.numerator
+        new.denominator = new.denominator * other.denominator
+        new.simplify()
+
+        print(f"MULTIPLIED: {new}")
+        return new
+
+    def simplify(self, in_place=True) -> 'Rational':
+        """
+        Simplifies this rational number and returns it. If in_place is set to true, it will simplify this object
+        """
+        my_gcd = math.gcd(self.numerator, self.denominator)
+        
+        new = Rational(self.numerator, self.denominator)
+        if my_gcd != 1:
+            new.numerator //= my_gcd
+            new.denominator //= my_gcd
+
+        if in_place:
+            self.numerator = new.numerator
+            self.denominator = new.denominator
+
+        return new
+
+    def __change_denominator(self, new_denominator: int):
+        """ 
+            change denominator to a new denominator, while preserving the 
+            rational number. Does not type check as it is an internal function
+
+            Args:
+                new_denominator [int]: The new denominator for the fraction
+            Returns:
+                [Rational]: A new Rational number representing the changed denominator 
+            Raises:
+                Exception if the denominator cannot be set and remain a rational number in the format p/q where both p and q are integers
+        """
+        lower = min(self.denominator, new_denominator)
+        higher = max(self.denominator, new_denominator)
+
+        if higher % lower != 0:
+            raise Exception(f"Cannot raise fraction {self} to denominator {new_denominator} without creating non-integer numerator")
+
+        multiplier = new_denominator // self.denominator
+
+        print(f"MULTIPLIER: {multiplier}")
+
+        # same denominator
+        if multiplier == 1:
+            new_numerator = self.numerator
+        else:
+            new_numerator = self.numerator * multiplier
+
+        return Rational(new_numerator, new_denominator)
+        
+
+    def __lcm(self, a: int, b: int) -> int:
+        """ Finds the lowest common multiple of 2 integers numbers"""
+        if a == b:
+            return a
+        else:
+            return (a*b) // math.gcd(a,b)
+
+
+    ## OVERRIDES:
+    def __repr__(self):
+        """ string format a/b or a if b = 1"""
+        return "({0}{1})".format(self.numerator, '/' + str(self.denominator) if self.denominator != 1 else '')
+
+    # ==
+    def __eq__(self, other: Type['Rational']) -> bool:
+        """ 
+        Override of ==. Will check for equality between fractions.
+
+        Returns true if the fractions represent the same rational number (i.e. 2/4 == 1/2 = True)
+        """
+        a = self.simplify(in_place=False)
+        b = other.simplify(in_place=False)
+        return a.numerator == b.numerator and a.denominator == b.denominator
+
+    # required for ==
+    def __hash__(self):
+        return object.__hash__(self)
+
+    # - 
+    def __sub__(self, other: Type['Rational']) -> 'Rational':
+        """ 
+        Subtract another rational number from this rational number RETURNS A NEW RATIONAL representing the subtraction
+
+        Args:
+            other [Rational]: The other rational to add to this one
+        Returns
+            [Rational]: Rational representing the subtraction
+        """
+        print("IN SUB")
+        return self.__subtract(other, in_place=False)
+
+    # -=
+    def __isub__(self, other: Type['Rational']) -> 'Rational':
+        """ 
+        Subtract another rational number from this rational number IN PLACE
+
+        Args:
+            other [Rational]: The other rational to add to this one
+        Returns
+            [Rational]: self after subtraction
+        """
+        self.__subtract(other, in_place=True)
+        return self
+
+    # *=
+    def __imul__(self, other: Type['Rational']) -> 'Rational':
+        """ 
+        Multiply another rational number to this rational number IN PLACE
+
+        Args:
+            other [Rational]: The other rational to multiply with this one
+        Returns
+            [Rational]: self after multiplication
+        """
+        return self.__multiply(other, in_place=True)
+
+    # * left multiply
+    def __mul__(self, other: Type['Rational']) -> 'Rational':
+        """ 
+        Multiply another rational number to this rational number RETURNS A NEW RATIONAL representing the product
+
+        Args:
+            other [Rational]: The other rational to multiply with this one
+        Returns
+            [Rational]: Rational representing the multiplication
+        """
+        return self.__multiply(other, in_place=False)
+
+    # * right multiply - not used but needs to be overridden to use mul
+    def __rmul__(self, other: Type['Rational']) -> 'Rational':
+        """ 
+        Multiply another rational number to this rational number RETURNS A NEW RATIONAL representing the product
+
+        Args:
+            other [Rational]: The other rational to multiply with this one
+        Returns
+            [Rational]: Rational representing the multiplication
+        """
+        return self.__multiply(other, in_place=False)
+
+    # /=
+    def __itruediv__(self, other: Type['Rational']) -> 'Rational':
+        """ 
+        Divide this rational number by other IN PLACE
+
+        Args:
+            other [Rational]: The other rational to multiply with this one
+        Returns
+            [Rational]: self after division
+        """
+        return self.__divide(other, in_place=True)
+
+    # /=
+    def __ifloordiv__(self, other: Type['Rational']) -> 'Rational':
+        """ 
+        identical implementation to itruediv
+        """
+        return self.__divide(other, in_place=True)
+
+    # /
+    def __truediv__(self, other: Type['Rational']) -> 'Rational':
+        """ 
+        Divie this rational number by other RETURNS A NEW RATIONAL representing the division
+
+        Args:
+            other [Rational]: The other rational to divide with this one
+        Returns
+            [Rational]: Rational representing the division
+        """
+        return self.__divide(other, in_place=False)
+
+    # /
+    def __floordiv__(self, other: Type['Rational']) -> 'Rational':
+        """ 
+        identical implementation to truediv
+        """
+        return self.__divide(other, in_place=False)
+
+    # +
+    def __add__(self, other: Type['Rational']) -> 'Rational':
+        """ 
+        Add another rational number to this rational number RETURNS A NEW RATIONAL representing the summation
+
+        Args:
+            other [Rational]: The other rational to add to this one
+        Returns
+            [Rational]: Rational representing the summation
+        """
+        return self.__add(other, in_place=False)
+
+    # +=
+    def __iadd__(self, other: Type['Rational']) -> 'Rational':
+        """ 
+        Add another rational number to this rational number IN PLACE
+
+        Args:
+            other [Rational]: The other rational to add to this one
+        Returns
+            [Rational]: self after addition
+        """
+        return self.__add(other, in_place=True)
---
+++ tests/test_rational.py
@@ -0,0 +1,155 @@
+from decimal import DivisionByZero
+import pytest
+
+import operator
+from ..rational import Rational
+
+def test_init():
+    exception_cases = [
+        (1, 0),     # denominator cannot be 0
+        (None, 1),  # numerator cannot be none
+        (1, -1),    # denominator cannot be negative
+        (0.5, 1),   # numerator cannot be float
+        (1, 0.5)    # denominator cannot be float
+    ]
+
+    number = Rational(1, 1)
+    assert number.numerator == 1
+    assert number.denominator == 1
+
+    number = Rational(-1, 5)
+    assert number.numerator == -1
+    assert number.denominator == 5
+
+    for case in exception_cases:
+        with pytest.raises(Exception):
+            number = Rational(case[0], case[1])
+
+def assert_iop_cases(cases, operation):
+    for case, expected in cases.items():
+        op = Rational(case[0].numerator, case[0].denominator)
+        operand = Rational(case[1].numerator, case[1].denominator)
+        operation(op, operand)
+        assert op.numerator == expected[0], f"numerator : {op.numerator} did not match expected: {expected[0]}"
+        assert op.denominator == expected[1], f"denominator : {op.denominator} did not match expected: {expected[1]}"
+
+def assert_op_cases(cases, operation):
+    for case, expected in cases.items():
+        op = Rational(case[0].numerator, case[0].denominator)
+        operand = Rational(case[1].numerator, case[1].denominator)
+        number = operation(op, operand)
+        assert number.numerator == expected[0], f"numerator : {number.numerator} did not match expected: {expected[0]}"
+        assert number.denominator == expected[1], f"denominator : {number.denominator} did not match expected: {expected[1]}"
+
+def test_simplify():
+    """
+    To test simplify, we create a bunch of rationals that can be simplified (or not)
+    and add 0. The add function simplifies after addition.
+    """
+    cases = {
+        (Rational(2, 4), Rational(0, 1)): (1, 2),
+        (Rational(5, 6), Rational(0, 1)): (5, 6),
+        (Rational(9, 27), Rational(0, 1)): (1, 3),
+        (Rational(18, 3), Rational(0, 1)): (6, 1),
+        (Rational(5, 1), Rational(0, 1)): (5, 1)
+    }
+
+    assert_op_cases(cases, operator.add)
+
+def test_divide():
+    """
+    Test adding 2 rationals using the + operator
+    """
+    cases = {
+        (Rational(2, 4), Rational(1, 1)): (1, 2),
+        (Rational(5, 6), Rational(1, 4)): (10, 3),
+        (Rational(1, 9), Rational(48, 54)): (1, 8),
+        (Rational(1, 9), Rational(21, 54)): (2, 7),
+        (Rational(18, 3), Rational(1, 8)): (48, 1),
+        (Rational(5, 1), Rational(-1, 1)): (-5, 1),
+        (Rational(-1, 5), Rational(-1, 1)): (1, 5),
+        (Rational(-18, 3), Rational(1, 8)): (-48, 1)
+    }
+    
+    assert_op_cases(cases, operator.truediv)
+    assert_op_cases(cases, operator.floordiv)
+    assert_op_cases(cases, operator.itruediv)
+    assert_op_cases(cases, operator.ifloordiv)
+
+    rat1 = Rational(1,1)
+    zero_rational = Rational(0,1)
+
+    with pytest.raises(DivisionByZero):
+        rat1 /= zero_rational
+
+    with pytest.raises(DivisionByZero):
+        new = rat1 / zero_rational
+
+
+def test_eq():
+    cases = {
+        (Rational(2, 4), Rational(1, 1)): False,
+        (Rational(5, 6), Rational(-5, 6)): False,
+        (Rational(-1, 2), Rational(1, 2)): False,
+        (Rational(1, 2), Rational(1, 2)): True,
+        (Rational(32, 2), Rational(16, 1)): True,
+        (Rational(-5, 2), Rational(-10, 4)): True,
+    }
+
+    for case, expected in cases.items():
+        assert (case[0] == case[1]) == expected, f"Failed equality assertion. {case[0]} == {case[1]} returned {not expected}, but expected {expected}"
+
+def test_mul():
+    """
+    Test adding 2 rationals using the + operator
+    """
+    cases = {
+        (Rational(2, 4), Rational(1, 1)): (1, 2),
+        (Rational(5, 6), Rational(1, 4)): (5, 24),
+        (Rational(1, 9), Rational(48, 54)): (8, 81),
+        (Rational(1, 9), Rational(21, 54)): (7, 162),
+        (Rational(18, 3), Rational(1, 8)): (3, 4),
+        (Rational(5, 1), Rational(0, 1)): (0, 1),
+        (Rational(5, 1), Rational(-1, 1)): (-5, 1),
+        (Rational(-1, 5), Rational(-1, 1)): (1, 5),
+        (Rational(-18, 3), Rational(1, 8)): (-3, 4)
+    }
+    
+    assert_op_cases(cases, operator.mul)
+    assert_op_cases(cases, operator.imul)
+
+def test_add():
+    """
+    Test adding 2 rationals using the + operator
+    """
+    cases = {
+        (Rational(2, 4), Rational(1, 1)): (3, 2),
+        (Rational(5, 6), Rational(18, 3)): (41, 6),
+        (Rational(1, 9), Rational(48, 54)): (1, 1),
+        (Rational(1, 9), Rational(21, 54)): (1, 2),
+        (Rational(18, 3), Rational(1, 8)): (49, 8),
+        (Rational(5, 1), Rational(0, 1)): (5, 1),
+        (Rational(5, 1), Rational(-1, 1)): (4, 1),
+        (Rational(1, 5), Rational(-1, 1)): (-4, 5)
+    }
+    
+    assert_op_cases(cases, operator.add)
+    assert_iop_cases(cases, operator.iadd)
+
+def test_subtract():
+    """
+    Test adding 2 rationals using the - operator
+    """
+    cases = {
+        (Rational(2, 4), Rational(1, 1)): (-1, 2),
+        (Rational(5, 6), Rational(18, 3)): (-31, 6),
+        (Rational(1, 9), Rational(48, 54)): (-7, 9),
+        (Rational(1, 9), Rational(21, 54)): (-5, 18),
+        (Rational(18, 3), Rational(-1, 8)): (49, 8),
+        (Rational(5, 1), Rational(0, 1)): (5, 1),
+        (Rational(5, 1), Rational(1, 1)): (4, 1),
+        (Rational(1, 5), Rational(1, 1)): (-4, 5)
+    }
+    
+    assert_op_cases(cases, operator.sub)
+    assert_op_cases(cases, operator.isub)
