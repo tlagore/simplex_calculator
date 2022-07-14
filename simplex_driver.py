1f347adea74f08487afb6e4e5d611800b82bccb1
Tyrone Lagore <tyronelagore@gmail.com>
2022-07-14 09:36:55 -0700

    to_string being called again

--- simplex_driver.py
+++ simplex_driver.py
@@ -1,6 +1,7 @@
 import sys
 import simplex.simplex_parser as sp
 from simplex.simplex_dictionary import SimplexConfig, PivotMethod, InitializationFn
+import cProfile
 
 def main():
     debug = False
@@ -11,19 +12,23 @@ def main():
 
     # Set configurations for simplex program
     # Defaults are LARGEST_INCREASE and FIBONNACI initialization (for substituted dual objective function)
-    simplex_config = SimplexConfig()
-    simplex_config.pivot_method = PivotMethod.LARGEST_COEFFICIENT
-    simplex_config.initialization_function = InitializationFn.FIBONNACI
-    solver = sp.parse(sys.stdin, simplex_config)
-    sys.stderr.write("Beginning solve...\n")
+    
 
-    if debug:
-        sys.stderr.write("Starting dictionary:\n")
-        sys.stderr.write("{0}\n".format(solver.s_dict.to_string()))
-        solver.enable_debug()
+    cProfile.run(
+"""
+debug = False
+simplex_config = SimplexConfig()
+simplex_config.pivot_method = PivotMethod.LARGEST_COEFFICIENT
+simplex_config.initialization_function = InitializationFn.FIBONNACI
+solver = sp.parse(sys.stdin, simplex_config)
 
-    solver.solve()
-    solver.stats.print_stats()
+if debug:
+    solver.enable_debug()
+
+solver.solve()
+solver.stats.print_stats()
+"""
+    )
 
 if __name__ == "__main__":
     main()
